a6272b246c78c63d4c8f67b6c78ebf132e3f4c09
jkjorgie <jkjorgie@yahoo.com>
2024-04-28 10:05:35 -0600

    jkj js event handling la

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -2,9 +2,19 @@ let input = document.querySelector('#favchap');
 let btn = document.querySelector('button');
 let list = document.querySelector('#list');
 
-const li = document.createElement('li');
-const delBtn = document.createElement('button');
-li.textContent = input.value;
-delBtn.textContent = '❌';
-li.append(delBtn);
-list.append(li);
+btn.addEventListener('click', function () {
+    if (input.value.trim() !== '') {
+        const li = document.createElement('li');
+        const delBtn = document.createElement('button');
+        li.textContent = input.value;
+        delBtn.textContent = '❌';
+        delBtn.addEventListener('click', function() {
+            list.removeChild(li);
+            input.focus();
+        });
+        li.append(delBtn);
+        list.append(li);
+        input.value = '';
+        input.focus();
+    }
+});
